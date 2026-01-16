d6505dc0948d9dd6d7362978c5d4472828f36764
e-afzal <essam.afzal@outlook.com>
2026-01-16 17:58:37 +0400

    Navbar added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+// LOCAL COMPONENTS
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Welcome to React Router!", index: false },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your applications & Resume Ratings</h1>
+          <h2>Review yor submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
