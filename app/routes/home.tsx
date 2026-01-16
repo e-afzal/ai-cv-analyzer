5af5f683539121e6e1982de31f036e444b1b6463
e-afzal <essam.afzal@outlook.com>
2026-01-16 21:57:02 +0400

    ResumeCards added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
 
 // LOCAL COMPONENTS
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,11 +16,18 @@ export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
-      <section className="main-section">
+      <section className="main-section py-16">
         <div className="page-heading">
           <h1>Track your applications & Resume Ratings</h1>
           <h2>Review yor submissions and check AI-powered feedback.</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
